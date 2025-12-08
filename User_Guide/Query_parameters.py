9130ddbc051d0b4509b1b4b31dfb46bd8d78d0d8
Sambath Raj <137758810+Sambathraj-Tech@users.noreply.github.com>
2025-12-08 12:42:17 +0530

    commit

--- User_Guide/Query_parameters.py
+++ User_Guide/Query_parameters.py
@@ -1,4 +1,4 @@
-# Example - 1
+# Example - 1: Default parameter
 
 fake_items_db = [
     {'item_name' : 'Foo'}, {'item_name' : 'Bar'}, {'item_name' : 'Baz'}
@@ -8,7 +8,7 @@ fake_items_db = [
 async def read_item(skip: int = 0, limit: int = 10):
     return fake_items_db[skip : skip + limit]
 -------------------------------------------------------------------------------------
-# Example - 2
+# Example - 2: Optional parameter
 
 @app.get("/items/{item_id}")
 async def read_items(item_id: str, q: str | None = None):
@@ -17,5 +17,47 @@ async def read_items(item_id: str, q: str | None = None):
                  "q" : q}
     return {"item_id" : item_id}
 -------------------------------------------------------------------------------------
-# Example - 3
+# Example - 3: Query Parameter Type Conversion
 
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+http://127.0.0.1:8000/items/foo?short=1
+or
+http://127.0.0.1:8000/items/foo?short=yes
+or
+http://127.0.0.1:8000/items/foo?short=on
+-------------------------------------------------------------------------------------
+# Example - 4: Multiple Path and Query Parameters
+
+
+@app.get("/users/{user_id}/items/{item_id}/")
+async def read_users(
+    user_id: int, item_id: str, query: str | None = None, short :bool = False
+):
+    item = {"item_id" : item_id, "owner_id" : user_id}
+    if query:
+        item.update({"Query_name" : query})
+    if not short:
+        item.update({"Inbuild" : "Own_text"})
+    return item
+-------------------------------------------------------------------------------------
+# Example - 5: Required query parameters
+
+@app.get("/items/{item_id}/")
+async def read_users(item_id: str, needy: str):
+    item = {"item_id" : item_id, "needy" : needy}
+    return item
