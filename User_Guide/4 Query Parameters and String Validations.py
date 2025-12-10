82315d4964e40f702141b8a120da7bf1c549c9c0
Sambath Raj <137758810+Sambathraj-Tech@users.noreply.github.com>
2025-12-10 11:25:25 +0530

    c

---
+++ User_Guide/4 Query Parameters and String Validations.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# FastAPI allows you to declare additional information and validation for your parameters.
+@app.get("/items")
+async def read_items(q: str | None = None):
+    result = {"items": [{"item_id" : "foo"}, {"item_id" : "Bar"}]}
+    if q:
+        result.update({"item_id" : "Baz"})
+    return result
+
+# Additional validation
+# Even though q is optional, whenever it is provided, its length doesn't exceed 50 characters.
+
+----------------------------------------------------------------------------------------------
+# Import Query and Annotated
+
+from typing import Annotated
+from fastapi import Query
+
+@app.get("/items1")
+async def read_items(q: Annotated[str | None , Query(max_length = 50)] = None):
+    result = {"items": [{"item_id" : "foo"}, {"item_id" : "Bar"}]}
+    if q:
+        result.update({"item_id" : "Baz"})
+    return result
