832c0605ec58221c4a39a6af40a8fc471e1d24ec
Sambath Raj <137758810+Sambathraj-Tech@users.noreply.github.com>
2025-12-10 10:48:14 +0530

    commit

--- User_Guide/Request_Body.py
+++ User_Guide/Request_Body.py
@@ -11,7 +11,24 @@ class Item(BaseModel):
 
 app = FastAPI()
 
+# Base Model
+
 @app.post("/items")
 async def create_items(item: Item):
     return item
+    
 ============================================================
+# Use the model
+@app.post("/itemss")
+async def update_items(item: Item):
+    item_dict = item.dict()
+    if item.tax is not None:
+        price_with_tax = item.tax + item.price
+        item_dict.update({"price_with_tax" : price_with_tax})
+    return item_dict
+    
+============================================================
+# Request body + path parameters
+@app.post("/users")
+async def update_item(item_id: int, item: Item):
+    return {"item_id" : item_id, **item.dict()}
